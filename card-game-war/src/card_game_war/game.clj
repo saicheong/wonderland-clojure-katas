c38ca11fd813e3c7b4452d39a22c9bddcab69f08
saicheong <yow.saicheong@gmail.com>
2017-05-18 22:12:28 +0800

    Add working solution to card-game-war
    Include support for more than 2 players.

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,64 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn value
+  "Returns the value of card that can be used for comparison and sorting."
+  [card]
+  [(.indexOf ranks (nth card 1)) (.indexOf suits (nth card 0))])
 
-(defn play-game [player1-cards player2-cards])
+(defn higher? [higher-card lower-card]
+  (> (compare (value higher-card) (value lower-card)) 0))
+
+(defn highest-value
+  "Returns the highest value card"
+  [cards]
+  (last (sort #(compare (value %1) (value %2)) cards)))
+
+(defn deal
+  "Distribute cards evenly into n vectors.
+  A deck of 52 cards will be distributed into 3 decks of 18 17 17 cards respectively"
+  [n cards]
+  (loop [idx 0                                              ; track which hand is to receive the next card
+         cards (seq cards)
+         ;; todo - find a better way to do this
+         hands (into [] (map (fn [i] (vector)) (range n)))]
+    (if cards
+      (let [card (first cards)
+            hand (nth hands idx)
+            new-hand (conj hand card)]
+        (recur (rem (inc idx) n) (next cards) (assoc hands idx new-hand)))
+      hands)))
+
+;;
+;; Added support for more than 2 players, just for fun
+;; ---------------------------------------------------
+
+(defn play-round
+  "Returns the index of the winning card"
+  ([c1 c2] (if (< (compare (value c1) (value c2)) 0) 1 0))
+  ([c1 c2 & more]
+    (let [cs (conj more c2 c1)
+          highest (highest-value cs)]
+      (first (keep-indexed #(if (= highest %2) %1) cs)))))
+
+(defn play-game
+  "Accepts 2 or more arrays of cards. The game ends when one or more players
+  have no more cards"
+  [cards1 cards2 & more]
+  (let [players (conj more cards2 cards1)
+        losers (keep-indexed #(when (= (count %2) 0) %1) players)]
+    (println (map count players))
+    (if (> (count losers) 0)
+      (do (println "Player"
+                   (map inc losers)
+                   (if (= 1 (count losers)) "is" "are")
+                   "out!")
+          losers)
+      ;otherwise play another round
+      (let [cards (into [] (map peek players))              ; collect cards from all
+            winner (apply play-round cards)
+            winnings (shuffle cards)]                       ; shuffle winnings
+        (->> players
+             (map pop)                                      ; remove played cards
+             (map-indexed #(if (not= winner %1) %2 (apply conj winnings %2)))
+             (apply play-game))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,43 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= 0
+           (play-round [:heart :ace] [:club :king])))
+    (is (= 1
+           (play-round [:club :king] [:heart :ace])))
+    (is (= 1
+           (play-round [:club :king] [:heart :ace] [:club :ace]))))
+
+  (testing "queens are higher rank than jacks"
+    (is (= true
+           (higher? [:spade :queen] [:club :jack]))))
+
+  (testing "kings are higher rank than queens"
+    (is (= true
+           (higher? [:spade :king] [:club :queen]))))
+
+  (testing "aces are higher rank than kings"
+    (is (= true
+           (higher? [:spade :ace] [:club :king]))))
+
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= true
+           (higher? [:club 10] [:spade 10]))))
+
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= true
+           (higher? [:diamond 10] [:club 10]))))
+
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= true
+           (higher? [:heart 10] [:diamond 10])))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (= '(0)
+           (play-game [] [[:heart 10]])))
+    (is (= '(1)
+           (play-game [[:heart 10]] [])))
+    (is (= '(0 2)
+           (play-game [] [[:heart 10]] [])))))
 
